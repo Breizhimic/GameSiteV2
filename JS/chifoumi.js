aa2ab740a5f91449bc7f76320c746c7c0e3dfcae
Breizhimic <ewen.parel@laposte.net>
2024-03-10 17:30:04 +0100

    x

---
+++ JS/chifoumi.js
@@ -0,0 +1,17 @@
+const imagePierre = document.getElementById("pierre");
+const imagePapier = document.getElementById("papier");
+const imageCiseaux = document.getElementById("ciseaux");
+const message = document.getElementById("message")
+const rejouer = document.getElementById("rejouer")
+
+rejouer.addEventListener( "click", function(){init();})
+
+let choixOrdi
+const choixPossible = ["PIERRE", "PAPIER", "CISEAUX"]
+
+function init(){
+    choixOrdi = ((Math.random()*2)).toFixed(0);
+    message.innerHTML= "Le choix de l'odinateur est fait, à vous de cliquez sur votre choix...";
+}
+
+init();
